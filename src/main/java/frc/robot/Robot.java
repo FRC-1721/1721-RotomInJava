87e24745939248634e4b3742ccfcff602b2368df
dublUayaychtee <mail@dublu.net>
2025-07-12 19:04:13 -0400

    我不喜欢匈牙利吧

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,23 +6,23 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(1);
-  private final PWMSparkMax m_leftMotor1 = new PWMSparkMax(2);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(3);
-  private final PWMSparkMax m_rightMotor1 = new PWMSparkMax(4);
+  private final PWMSparkMax leftMotor = new PWMSparkMax(1);
+  private final PWMSparkMax leftMotor1 = new PWMSparkMax(2);
+  private final PWMSparkMax rightMotor = new PWMSparkMax(3);
+  private final PWMSparkMax rightMotor1 = new PWMSparkMax(4);
 
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
-  private final Joystick m_stick = new Joystick(0);
+  private final DifferentialDrive robotDrive = new DifferentialDrive(leftMotor::set, rightMotor::set);
+  private final Joystick stick = new Joystick(0);
 
   public Robot() {
-    m_leftMotor.addFollower(m_leftMotor1);
-    m_rightMotor.addFollower(m_rightMotor1);
+    leftMotor.addFollower(leftMotor1);
+    rightMotor.addFollower(rightMotor1);
 
-    m_rightMotor.setInverted(true);
+    rightMotor.setInverted(true);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    robotDrive.arcadeDrive(-stick.getY(), -stick.getX());
   }
 }
