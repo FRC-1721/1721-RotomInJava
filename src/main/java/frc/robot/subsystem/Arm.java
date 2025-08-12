d7a48dc221bfc57a9fd9158a1cea71867dffd26d
dublUayaychtee <mail@dublu.net>
2025-08-12 11:02:24 -0400

    forgot to add this file

--- src/main/java/frc/robot/subsystem/Arm.java
+++ src/main/java/frc/robot/subsystem/Arm.java
@@ -1,17 +1,25 @@
 package frc.robot.subsystem;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Arm extends SubsystemBase{
-  private final SparkMax claw = new SparkMax(Constants.MotorConstants.claw, MotorType.kBrushless);
+  //private final SparkMax claw = new SparkMax(Constants.MotorConstants.claw, MotorType.kBrushless);
   private final SparkMax ladder = new SparkMax(Constants.MotorConstants.ladder, MotorType.kBrushless);
   private final SparkMax elevator = new SparkMax(Constants.MotorConstants.elevator, MotorType.kBrushless);
 
-  public Arm() {}
+  public Arm() {
+    SparkMaxConfig ladderConfig = new SparkMaxConfig();
+    ladderConfig.idleMode(IdleMode.kBrake);
+    ladder.configure(ladderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
 
   @Override
   public void periodic() {}
@@ -22,7 +30,7 @@ public class Arm extends SubsystemBase{
    * @param speed Motor speed
    */
   public void extension(double speed) {
-    elevator.set(speed);
+    elevator.set(speed * Constants.DriveConstants.DampenElevator);
   }
 
   /**
@@ -32,10 +40,10 @@ public class Arm extends SubsystemBase{
    * @param speed Motor speed
    */
   public void ascent(double speed) {
-    ladder.set(speed);
+    ladder.set(speed * Constants.DriveConstants.DampenLadder);
   }
 
   public void grab(double speed) {
-    claw.set(speed);
+    //    claw.set(speed);
   }
 }
