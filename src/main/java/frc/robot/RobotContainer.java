d3eaca7a364647aba2b126f544c3ccd5ed895ccc
dublUayaychtee <mail@dublu.net>
2025-08-12 10:59:17 -0400

    add Arm command as default; correct drive axis direction

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,15 @@
 package frc.robot;
 
 import frc.robot.command.ArcadeDrive;
+import frc.robot.command.ManualArm;
 import frc.robot.subsystem.Drive;
+import frc.robot.subsystem.Arm;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
   private final Drive driveSubsystem = new Drive();
+  private final Arm armSubsystem = new Arm();
 
   private final CommandXboxController driverController =
       new CommandXboxController(Constants.OperatorConstants.DriverControllerPort);
@@ -15,7 +18,10 @@ public class RobotContainer {
     configureBindings();
 
     driveSubsystem.setDefaultCommand(
-      new ArcadeDrive(driveSubsystem, () -> driverController.getRawAxis(1), () -> driverController.getRawAxis(0))
+      new ArcadeDrive(driveSubsystem, () -> -driverController.getRawAxis(1), () -> driverController.getRawAxis(0))
+    );
+    armSubsystem.setDefaultCommand(
+      new ManualArm(armSubsystem, () -> -driverController.getRawAxis(5), () -> driverController.getRawAxis(3) - driverController.getRawAxis(2))
     );
   }
 
